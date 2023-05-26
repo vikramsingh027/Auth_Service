8193b2758f5c0938fa1bca71ad681b4507504b87
Vikramsingh Rajput <vikramsinghdrajput@gmail.com>
2023-05-26 12:54:37 +0530

    Added signIn Logic

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -24,6 +24,28 @@ const create = async (req, res) => {
   }
 };
 
+const signIn = async (req, res) => {
+  try {
+    const response = await userService.signIn(
+      req.body.email,
+      req.body.password
+    );
+    return res.status(200).json({
+      data: response,
+      message: "Successfully signed in",
+      success: true,
+      err: {},
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "something went wrong",
+      success: false,
+      data: {},
+      err: error,
+    });
+  }
+};
 module.exports = {
   create,
+  signIn,
 };
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,4 +1,5 @@
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const UserRepository = require("../repository/user-repository");
 const { JWT_KEY } = require("../config/serverConfig");
@@ -28,7 +29,36 @@ class UserService {
     }
   }
 
-  createToken(user) {
+  async signIn(email, plainPassword) {
+    try {
+      //step1 -> fetch user using email
+      const user = await this.userRepository.getByEmail(email);
+      if (!user) {
+        console.log("Email does not match");
+        throw { error: "This email does not exist" };
+      }
+      //step2 -> compare incoming plain password with stored encrypted password
+      const passwordsMatch = await this.#checkPassword(
+        plainPassword,
+        user.password
+      );
+      if (!passwordsMatch) {
+        console.log("Password doesn't match");
+        throw { error: "Incorrect Password" };
+      }
+      //step3 -> if passwords match then create a token and send it to the user
+      const newJWT = this.#createToken({
+        email: user.email,
+        id: user.id,
+      });
+      return newJWT;
+    } catch (error) {
+      console.log("Something went wrong in the signin process");
+      throw error;
+    }
+  }
+
+  #createToken(user) {
     try {
       const token = jwt.sign(user, JWT_KEY, { expiresIn: "1h" });
       return token;
@@ -44,6 +74,19 @@ class UserService {
       return response;
     } catch (error) {
       console.log("something went wrong in token verificaiton");
+      throw error;
+    }
+  }
+
+  async #checkPassword(userInputPlainPassword, encryptedPassword) {
+    try {
+      const response = await bcrypt.compare(
+        userInputPlainPassword,
+        encryptedPassword
+      );
+      return response;
+    } catch (error) {
+      console.log("something went wrong in password comparison");
       throw error;
     }
   }
